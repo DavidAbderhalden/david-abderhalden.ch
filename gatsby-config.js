76fe9a5b4de84805090c83ba79a83a5ca977cb6a
David Abderhalden <david.abderhalden@hotmail.com>
2021-11-22 23:11:39 +0100

    Gatsby setup
    Sass Loader / React Helmet / Manifest Plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,22 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.david-abderhalden.ch",
     title: "David-Abderhalden",
   },
   plugins: [
-    "gatsby-plugin-sass",
+    "gatsby-plugin-sass", 
     "gatsby-plugin-react-helmet",
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        icon: "src/images/icon.png",
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/assets/img/favicon.png`,
       },
-    },
+    }
   ],
 };
---
+++ src/components/atoms/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+import '../../styles/atoms/Header.scss'
+
+export default function Header() {
+    return (
+        <div class="test">
+
+        </div>
+    )
+}
